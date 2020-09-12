b66f04c366753e533bd869a9b922ba15fd1b0850
Taras Chuiko <taraschuiko@gmail.com>
2020-09-12 16:41:30 +0300

    Completed the code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+#define relay 2
+#define distanceTrig 9
+#define distanceEcho 10
 
+long duration;
+long distance;
+
+// Configurations
+int dispenseDuration = 200;
+int waitAfterDispense = 2000;
+int dispenseWhenDistanceIsLowerThan = 14;
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  pinMode(distanceTrig, OUTPUT);
+  pinMode(distanceEcho, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(relay, HIGH);
+  
+  digitalWrite(distanceTrig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(distanceTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(distanceTrig, LOW);
 
+  duration = pulseIn(distanceEcho, HIGH);
+  distance = (duration/2) / 29.1;
+
+  Serial.print(distance);
+  Serial.println("cm");
+
+  if (distance < dispenseWhenDistanceIsLowerThan) {
+    digitalWrite(relay, LOW);
+    delay(dispenseDuration);
+    digitalWrite(relay, HIGH);
+    delay(waitAfterDispense);
+  }
+
+  delay(500);
 }
